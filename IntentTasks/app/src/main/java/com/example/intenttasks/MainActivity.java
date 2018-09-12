ec8c5058c2b9f16ebab1a41f8ec1679e65724786
Aina <ainaard@mail.ru>
2018-09-13 00:05:24 +0300

    intent added

--- IntentTasks/app/src/main/java/com/example/intenttasks/MainActivity.java
+++ IntentTasks/app/src/main/java/com/example/intenttasks/MainActivity.java
@@ -1,13 +1,37 @@
 package com.example.intenttasks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView email;
+    TextView password;
+    TextView phoneNumber;
+    ImageButton profilePicButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        phoneNumber = findViewById(R.id.phoneNumber);
+        profilePicButton = findViewById(R.id.profilePic);
+
+        profilePicButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
