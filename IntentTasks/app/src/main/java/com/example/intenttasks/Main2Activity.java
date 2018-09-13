f87fe7f468731686216ebe0c86506d0827852056
Aina <ainaard@mail.ru>
2018-09-13 22:44:42 +0300

    activities, intents are added

--- IntentTasks/app/src/main/java/com/example/intenttasks/Main2Activity.java
+++ IntentTasks/app/src/main/java/com/example/intenttasks/Main2Activity.java
@@ -1,13 +1,71 @@
 package com.example.intenttasks;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class Main2Activity extends AppCompatActivity {
+
+    TextView emailEdit;
+    TextView nameEdit;
+    TextView phoneNumberEdit;
+    ImageButton profilePicEdit;
+    Button confirmButton;
+    Button cancelButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        emailEdit = findViewById(R.id.emailEdit);
+        nameEdit = findViewById(R.id.nameEdit);
+        phoneNumberEdit = findViewById(R.id.phoneNumberEdit);
+        profilePicEdit = findViewById(R.id.profilePicEdit);
+        confirmButton = findViewById(R.id.confirmButton);
+        cancelButton = findViewById(R.id.cancelButton);
+
+        final String filledEmail = emailEdit.getText().toString();
+        final String filledName = nameEdit.getText().toString();
+        final String filledNumber = phoneNumberEdit.getText().toString();
+
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentPost = new Intent(Main2Activity.this, MainActivity.class);
+                intentPost.putExtra("email", filledEmail);
+                intentPost.putExtra("name", filledName);
+                intentPost.putExtra("number", filledNumber);
+
+                startActivity(intentPost);
+            }
+        });
+
+        cancelButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent cancelIntent = new Intent(Main2Activity.this,MainActivity.class);
+                Toast toast = Toast.makeText(getApplicationContext(),"Cancel the edit",Toast.LENGTH_LONG);
+                toast.show();
+                startActivity(cancelIntent);
+
+            }
+        });
+
+        profilePicEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent changePhotoIntent = new Intent(Intent.ACTION_PICK);
+                changePhotoIntent.setType("image/*");
+                startActivity(changePhotoIntent);
+            }
+        });
+
     }
 }
--- IntentTasks/app/src/main/java/com/example/intenttasks/MainActivity.java
+++ IntentTasks/app/src/main/java/com/example/intenttasks/MainActivity.java
@@ -1,37 +1,63 @@
 package com.example.intenttasks;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
     TextView email;
-    TextView password;
+    TextView name;
     TextView phoneNumber;
-    ImageButton profilePicButton;
+    ImageView profilePic;
+    Button editButton;
+    Button googleButton;
+
+    private static final int REQUEST_ID_URI = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        email = findViewById(R.id.email);
-        password = findViewById(R.id.password);
-        phoneNumber = findViewById(R.id.phoneNumber);
-        profilePicButton = findViewById(R.id.profilePic);
 
-        profilePicButton.setOnClickListener(new View.OnClickListener() {
+        profilePic = findViewById(R.id.profilePic);
+        editButton = findViewById(R.id.buttonEdit);
+        googleButton = findViewById(R.id.googleButton);
+
+        editButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                String filledEmail = getIntent().getStringExtra("email");
+                String filledName = getIntent().getStringExtra("name");
+                String filledNumber = getIntent().getStringExtra("number");
+                if (filledEmail != null && filledName != null && filledNumber != null) {
+                    email = findViewById(R.id.email);
+                    name = findViewById(R.id.name);
+                    phoneNumber = findViewById(R.id.phoneNumber);
+                    email.setText(filledEmail);
+                    name.setText(filledName);
+                    phoneNumber.setText(filledNumber);
+                }
                 startActivity(intent);
             }
         });
 
+        googleButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "http://google.com";
+                Intent intentGoogle = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivityForResult(intentGoogle,REQUEST_ID_URI);
+            }
+        });
 
     }
 }
